c9514ea6995ae170dde6584ea51079760b232c47
kevinene91 <kevinene91@gmail.com>
2018-10-15 22:34:00 +0300

    [Chore #161209990] test registration with already used email

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -1,7 +1,7 @@
 import pytest
 from ... import create_app
 
-from .dummy import users
+from .dummy import users, empty
 
 app = create_app(config_name="testing")
 client = app.test_client()
@@ -14,8 +14,15 @@ def test_register():
     assert 'registration sucessfull' in str(response.json)
 
 
+# test with empty data
 def test_register_without_data():
     response = client.post('/api/v1/register', json=empty)
     assert response.status_code == 400
     assert 'missing required field' in str(response.json)
 
+
+# test already used email
+def test_register():
+    response = client.post('/api/v1/register', json=users[0])
+    assert response.status_code == 409
+    assert 'user with email already registred' in str(response.json)
