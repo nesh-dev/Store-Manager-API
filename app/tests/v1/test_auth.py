d7d1c80bf847b02a502ca0f94f2a629a5a4c7ff9
kevinene91 <kevinene91@gmail.com>
2018-10-15 22:28:04 +0300

    [Chore #161209990] test registration with empty data

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -7,8 +7,15 @@ app = create_app(config_name="testing")
 client = app.test_client()
 
 
+# sucess register
 def test_register():
     response = client.post('/api/v1/register', json=users[0])
     assert response.status_code == 200
     assert 'registration sucessfull' in str(response.json)
+
+
+def test_register_without_data():
+    response = client.post('/api/v1/register', json=empty)
+    assert response.status_code == 400
+    assert 'missing required field' in str(response.json)
 
