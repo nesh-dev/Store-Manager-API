5dfd9680592f1dec4db47a892d41a3ac534b1e0c
kevinene91 <kevinene91@gmail.com>
2018-10-15 22:44:27 +0300

    [Chore #161209990] test login

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -22,7 +22,16 @@ def test_register_without_data():
 
 
 # test already used email
-def test_register():
+def test_register_existing_email():
     response = client.post('/api/v1/register', json=users[0])
     assert response.status_code == 409
     assert 'user with email already registred' in str(response.json)
+
+
+# test sucess login 
+def test_login():
+    response = client.post('/api/v1/login', json=users[0])
+    assert response.status_code == 200 
+    assert 'user logged in' in str(response.json)
+
+
