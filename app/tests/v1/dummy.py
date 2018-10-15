c07ec02860ec098c04be6feaf2a67af745cc0fe8
kevinene91 <kevinene91@gmail.com>
2018-10-15 21:42:11 +0300

    [Chore #161209990]add dummy data

---
+++ app/tests/v1/dummy.py
@@ -0,0 +1,21 @@
+"""
+    Has tests dummy data
+"""
+
+Users = [
+
+    {"username": "nesh", "email": "munenedeveloper@gmail.com", 
+        "password": "12345", "role": 1},
+    {"username": "nesh", "email": "ksksksk", "password": "12345", "role": 1},
+    {"username": "nesh", "email": "munenedeveloper@gmail.com",
+        "password": "123245", "role": 1},
+    {"email": "munenedeveloper@gmail.com",
+        "password": "123245", "role": 1},
+    {"username": "nesh", "password": "123245", "role": 1},
+    {"username": "nesh", "email": "munenedeveloper@gmail.com", "role": 1},
+    {"username": "nesh", "email": "munenedeveloper@gmail.com", 
+        "password": "12345"}
+
+]
+
+empty = {}
