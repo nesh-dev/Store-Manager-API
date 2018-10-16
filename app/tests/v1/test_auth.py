8e7f21038d1273ef8cf58001f026151b9f5e1f49
kevinene91 <kevinene91@gmail.com>
2018-10-16 08:14:42 +0300

    [Chore #161209990] test regsiter invalid email

--- app/tests/v1/test_auth.py
+++ app/tests/v1/test_auth.py
@@ -3,6 +3,12 @@ from ... import create_app
 
 from .dummy import users, empty
 
+
+"""
+    Tests for user auth
+"""
+
+
 # app instance as test
 app = create_app(config_name="testing")
 client = app.test_client()
@@ -26,6 +32,13 @@ def test_register_without_data():
 def test_register_existing_email():
     response = client.post('/api/v1/register', json=users[0])
     assert response.status_code == 409
+    assert 'user with email already registred' in str(response.json)
+
+
+# test resgister invalid email
+def test_register_invalid_email():
+    response = client.post('/api/v1/register', json=users[1])
+    assert response.status_code == 422
     assert 'user with email already registred' in str(response.json)
 
 
