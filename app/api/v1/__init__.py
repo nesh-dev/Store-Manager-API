b13dbfe47d6ee14648af1408de5d0b8e30b9d322
kevinene91 <kevinene91@gmail.com>
2018-10-18 00:11:52 +0300

    [Feature #161209954] get category by id

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -4,7 +4,7 @@ from flask_restful import Api
 
 from .views.auth import (RegisterResource, LoginResource, 
                          LogoutResource)
-from .views.category import CategoryListResource
+from .views.category import CategoryListResource, CategoryResource
 
 apiv1 = Blueprint('apiv1', __name__)
 
@@ -16,5 +16,6 @@ api.add_resource(RegisterResource, '/register')
 api.add_resource(LoginResource, '/login')
 api.add_resource(LogoutResource, '/logout')
 api.add_resource(CategoryListResource, '/categories')
+api.add_resource(CategoryResource, '/category/<int:id>')
 
 
--- app/api/v1/views/category.py
+++ app/api/v1/views/category.py
@@ -30,12 +30,25 @@ class CategoryListResource(Resource):
         if CategoryModel.get_by_name(data['name'], cat_list):
             return {"message": "category with name already exist"}, 409
 
-        category_input = {"name": data["name"], 
+        category_input = {"id":cat_id, "name": data["name"], 
                           "description": data["description"]}
         CategoryModel.add_category(category_input)
         category = CategoryModel.get_by_name(data['name'], cat_list)
         return category, 201
 
 
+class CategoryResource(Resource):
 
+    @attendant_auth
+    def get(self, id):
+        """
+            get by id
+        """
+        message = "category with id {} does not exist".format(id)
+        cat_list = CategoryModel.get_categories()
+        category = CategoryModel.get_by_id(id, cat_list)
+        if category:
+            return category, 200
+        return {"message": message}, 404
 
+    
