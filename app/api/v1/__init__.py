261a91fe20ca9f34322c2e8f1ff95a170e5d1d77
kevinene91 <kevinene91@gmail.com>
2018-10-18 10:44:30 +0300

    [Feature #161209876] add products feature

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -3,6 +3,7 @@ from flask import Blueprint
 from flask_restful import Api 
 
 from .views.auth import RegisterResource, LoginResource, LogoutResource
+from .views.products import ProductListResource, ProductResource
 
 apiv1 = Blueprint('apiv1', __name__)
 
@@ -13,5 +14,7 @@ api = Api(apiv1, prefix='/api/v1')
 api.add_resource(RegisterResource, '/register')
 api.add_resource(LoginResource, '/login')
 api.add_resource(LogoutResource, '/logout')
+api.add_resource(ProductListResource, '/products')
+api.add_resource(ProductResource, '/product/<int:id>')
 
 
--- app/api/v1/middleware/middleware.py
+++ app/api/v1/middleware/middleware.py
@@ -1,3 +1,4 @@
+
 from functools import wraps
 from flask import request
 from flask import jsonify, make_response, abort
@@ -5,26 +6,43 @@ from flask_jwt_extended import (jwt_required,
                                 verify_jwt_in_request, get_jwt_identity)
 
 
-def attendant_auth(fn):
+def admin_auth(fn):
     @wraps(fn)
     @jwt_required
     def wrapper(*args, **kwargs):
-        return fn(*args, **kwargs)
-    return wrapper
-
-
-def admin_auth(fn):
-    @wraps(fn)
-    @attendant_auth
-    def wrapper(*args, **kwargs):
         verify_jwt_in_request()
         claims = get_jwt_identity()
-        if claims[2] != 2:
+        if claims['role'] != 2:
             abort(
                 make_response(
                     jsonify({'message': 'unauthorized to perform  function'}),
                     401
                 )
             )
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def attendant_auth(fn):
+    @wraps(fn)
+    @jwt_required
+    def wrapper(*args, **kwargs):
+        verify_jwt_in_request()
+        claims = get_jwt_identity()
+        if claims['role'] != 1:
+            abort(
+                make_response(
+                    jsonify({'message': 'unauthorized to perform  function'}),
+                    401
+                )
+            )
+        return fn(*args, **kwargs)
+    return wrapper
+
+
+def both_auth(fn):
+    @wraps(fn)
+    @jwt_required
+    def wrapper(*args, **kwargs):
         return fn(*args, **kwargs)
     return wrapper
