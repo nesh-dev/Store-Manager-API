214d3b19c37b643367a6aa56d9c68d6e5f4094a6
kevinene91 <kevinene91@gmail.com>
2018-10-23 08:56:07 +0300

    [Feature #161379550] add auth tests

---
+++ app/tests/v2/dummy_data.py
@@ -0,0 +1,29 @@
+
+""" Users dummy data """
+users = [
+    {"username": "nesh", "email": "munenedeveloper@gmail.com",
+     "password": "123456", "confirm_password": "123456"},
+
+    {"username": "andela", "email": "andela@gmail.com",
+     "password": "123456", "confirm_password": "123456"},
+
+    {"username": "mary", "email": "mary@gmail.com",
+     "password": "123456", "confirm_password": "123456"},
+
+    {"username": "mary", "email": "marygmail.com",
+     "password": "123456", "confirm_password": "123456"},
+
+    {},
+
+    {"username": "mary", "email": "marygmail.com",
+     "password": "123456", "confirm_password": "12345996"},
+
+    {"username": "mary",
+     "password": "123456", "confirm_password": "12345996"},
+
+    {"username": "nesh", "email": "munenedeveloper@gmail.com",
+     "confirm_password": "123456"},
+
+    {"username": "", "email": "munenedeveloper@gmail.com",
+     "password": "123456", "confirm_password": "123456"},
+     ]
